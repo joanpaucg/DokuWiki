9094b2c469d23b6da0b447070b530901f9486fd3
joanpaucg <joanpau.castells@hotmail.com>
2017-11-30 15:44:37 +0100

    desplegament dokuwiki

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+jcg23jordi002:$1$Ytmfhvq2$2.dg7AeeTBBc2E81iW/dv.:Joan Pau Castells Jordi Gòdia:joanpau.castells@hotmail.com:admin,user
