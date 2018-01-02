12213c793b548f22355ca9a94beab264240b39c8
Jordi Godia <jgm17@alumnes.udl.cat>
2018-01-02 19:39:40 +0100

    Index creat

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -10,3 +10,5 @@
 
 
 jcg23jordi002:$1$Ytmfhvq2$2.dg7AeeTBBc2E81iW/dv.:Joan Pau Castells Jordi Gòdia:joanpau.castells@hotmail.com:admin,user
+jgm17:$1$6grklk79$EkIlsb6xJyxtWPqoxAcZg0:jordi gòdia:jgm17@alumnes.udl.cat:user
+prova:$1$A1fxd0bl$aXi8hJXjftvVLHCOkSc1n1:prova:adsd@asdas.com:user
